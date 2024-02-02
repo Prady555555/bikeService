1a61629af1aa56107c2ce0cd7766ca9c6871a440
Prady555555 <139098258+Prady555555@users.noreply.github.com>
2024-02-02 11:01:46 +0530

    app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import nodemailer from "nodemailer";
 import { IdConverter } from "./db.js";
 const app = express();
-const port = 3000;
+const port = process.env.PORT ||3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
